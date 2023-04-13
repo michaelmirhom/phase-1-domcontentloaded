0f2e5dd3756b0d75b581ce3052d7dbaefdef6d92
michaelmirhom <micharby73@gmail.com>
2023-04-13 13:06:34 -0500

    copmleted

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+/*document.addEventListener("DOMContentLoaded", function() {
+    const heading = document.querySelector("h1");
+    heading.textContent = "This is really cool!";
+  });*/
+  document.addEventListener("DOMContentLoaded", function() {
+    const text = document.querySelector("#text");
+    text.textContent = "This is really cool!";
+  });
